a258e698d37628ae9932494ecc2da1fd55ce316d
Joey Li <joeyzaozaoli@gmail.com>
2017-01-28 22:26:11 -0800

    refine findParent function

--- scripts/album.js
+++ scripts/album.js
@@ -63,10 +63,18 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
-            currentParent = currentParent.parentElement;
+        if (currentParent === null) {
+            console.log("No parent found");
+        } else {
+            while (currentParent.className !== targetClass && currentParent.className !== null) {
+                currentParent = currentParent.parentElement;
+            }
+            if (currentParent.className === null) {
+                console.log("No parent found with that class name");
+            } else {
+                return currentParent;
+            }
         }
-        return currentParent;
     }
 };
 
